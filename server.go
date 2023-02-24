b380e1d7d4c9fa07d1c66e46f582f1eac1b8974f
xcd0 <xcd0@xcd0.com>
2023-02-24 15:03:19 +0900

    うごいた

--- server.go
+++ server.go
@@ -54,17 +54,61 @@ func generateServer(config *Config) {
 
 	// リバースプロキシの生成
 	for _, rp := range config.reverse {
-		if _, err := url.Parse(fmt.Sprintf("http://localhost:%d/", rp.port)); err != nil {
+		proxyUrl, err := url.Parse(fmt.Sprintf("http://localhost:%d/", rp.port))
+		if err != nil {
 			log.Fatalf("invalid port number %d for reverse proxy %q: %v", rp.port, rp.in_dir, err)
 		}
-		http.Handle(
+
+		proxy := httputil.NewSingleHostReverseProxy(proxyUrl)
+
+		http.HandleFunc(
 			fmt.Sprintf("/%s/", rp.in_dir),
-			httputil.NewSingleHostReverseProxy(
-				&url.URL{
-					Scheme: "http",
-					Host:   fmt.Sprintf("localhost:%d", rp.port),
-				}),
-		)
+			func(w http.ResponseWriter, r *http.Request) {
+				// httpsは対応していないかんじ
+				// クライアントがリクエストしたURLを取得
+				log.Println("---------------------------------------------")
+				log.Printf("リクエストされたURL : %s", r.URL.String())
+
+				/*
+					originalURL := fmt.Sprintf("http://%s%s/", r.Host, r.RequestURI)
+					targetURL, err := url.Parse(originalURL)
+					if err != nil {
+						log.Println(err)
+						log.Printf("Error : 不正なURL : %s", originalURL)
+					}
+				*/
+				log_request(r)
+
+				log.Println("-- rewrite --")
+				// example.com/aaa/にアクセスがあったとき
+				// r.RequestURI と r.Pathの両方に/aaa/が入る
+				// localhost:8080/aaa/に飛ばすとき
+				// どうもそのままだとpath + requesturiしてしまうみたい
+
+				originalURL := "http://" + r.Host + r.RequestURI
+				target, _ := url.Parse(originalURL)
+				r.URL.Host = target.Host
+				r.URL.Scheme = target.Scheme
+				r.URL.Path = target.Path
+
+				r.URL.Host = r.Host // r.URL.HostにはリクエストされたURLを入れる
+				//r.Host = fmt.Sprintf("localhost:%d", rp.port) // r.HostにプロキシサーバーのURLを入れる
+
+				log_request(r)
+				//log.Printf("%s/%s/", r.URL.Host, rp.in_dir)
+
+				proxy.ServeHTTP(w, r)
+			})
+		/*
+			http.Handle(
+				fmt.Sprintf("/%s/", rp.in_dir),
+				httputil.NewSingleHostReverseProxy(
+					&url.URL{
+						Scheme: "http",
+						Host:   fmt.Sprintf("localhost:%d", rp.port),
+					}),
+			)
+		*/
 		if rp.out_dir == "/" {
 			log.Printf("proxying http://localhost/%s/ to http://localhost:%d/", rp.in_dir, rp.port)
 		} else {
@@ -73,4 +117,27 @@ func generateServer(config *Config) {
 	}
 
 	log.Fatal(http.ListenAndServe(":80", nil))
+}
+
+func log_request(r *http.Request) {
+	u := r.URL
+	log.Printf("r.Host: %s", r.Host)
+	log.Printf("r.RequestURI : %s", r.RequestURI)
+	log_url(u)
+}
+func log_url(u *url.URL) {
+	log.Printf(
+		"\nr.URL.Scheme : %v, \n r.URL.Opaque : %v, \n r.URL.User : %v, \n r.URL.Host : %v, \n r.URL.Path : %v, \n r.URL.RawPath : %v, \n r.URL.OmitHost : %v, \n r.URL.ForceQuery : %v, \n r.URL.RawQuery : %v, \n r.URL.Fragment : %v, \n r.URL.RawFragment : %v, \n ",
+		u.Scheme,
+		u.Opaque,
+		u.User,
+		u.Host,
+		u.Path,
+		u.RawPath,
+		u.OmitHost,
+		u.ForceQuery,
+		u.RawQuery,
+		u.Fragment,
+		u.RawFragment,
+	)
 }
